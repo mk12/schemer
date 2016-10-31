9edd578d6fd97c6628b27d50eb65477ddeb9b583
Mitchell Kember <mk12360@gmail.com>
2016-10-30 20:21:12 -0700

    Add file for The Little Prover

---
+++ prover.scm
@@ -0,0 +1,9 @@
+;;; Copyright 2016 Mitchell Kember. Subject to the MIT License.
+;;; (exercises from The Little Prover)
+
+;;;;; Chapter 1: Old Games, New Rules
+
+(atom (car (cons (car a) (cdr b))))
+;; => (atom (car a))
+
+;;;;; Chapter 2: Even Older Games
